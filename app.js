eacf6d1dd7a881a62279eab173b19b609b96eb84
aparnad7 <dondapatiaparna@outlook.com>
2022-11-30 23:20:45 -0600

    Committing for Assignment13

--- app.js
+++ app.js
@@ -4,6 +4,22 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 mongoose = require('mongoose');
+var passport = require('passport'); 
+var LocalStrategy = require('passport-local').Strategy; 
+ 
+passport.use(new LocalStrategy( 
+  function(username, password, done) { 
+    Account.findOne({ username: username }, function (err, user) { 
+      if (err) { return done(err); } 
+      if (!user) { 
+        return done(null, false, { message: 'Incorrect username.' }); 
+      } 
+      if (!user.validPassword(password)) { 
+        return done(null, false, { message: 'Incorrect password.' }); 
+      } 
+      return done(null, user); 
+    }); 
+  } ));
 
 require('dotenv').config();
 const connectionString =  process.env.MONGO_CON;
@@ -34,6 +50,22 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(require('express-session')({ 
+  secret: 'keyboard cat', 
+  resave: false, 
+  saveUninitialized: false 
+})); 
+app.use(passport.initialize()); 
+app.use(passport.session()); 
+
+// passport config 
+// Use the existing connection 
+// The Account model  
+var Account =require('./models/account'); 
+ 
+passport.use(new LocalStrategy(Account.authenticate())); 
+passport.serializeUser(Account.serializeUser()); 
+passport.deserializeUser(Account.deserializeUser()); 
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/resource.js
+++ routes/resource.js
@@ -5,6 +5,14 @@ var router = express.Router();
 var api_controller = require('../controllers/api'); 
 var Animal_controller = require('../controllers/Animal'); 
 
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
+
 /// API ROUTE /// 
 
 // GET resources base. 
@@ -27,10 +35,12 @@ router.get('/Animal/:id', Animal_controller.Animal_detail);
 // GET request for list of all Animal items. 
 router.get('/Animal', Animal_controller.Animal_list); 
 
-router.get('/detail', Animal_controller.Animal_view_one_Page);
+router.get('/detail',secured, Animal_controller.Animal_view_one_Page);
 
-router.get('/create',Animal_controller.Animal_create_Page);
+router.get('/create',secured,Animal_controller.Animal_create_Page);
 
-router.get('/update', Animal_controller.Animal_update_Page);
+router.get('/update',secured, Animal_controller.Animal_update_Page);
+
+router.get('/delete',secured, Animal_controller.Animal_delete_Page);
 
 module.exports = router; 
